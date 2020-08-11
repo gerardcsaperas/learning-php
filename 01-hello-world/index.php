e3c07da838aa252d6d8adad2a707c6605ea14d84
Gerard C. Saperas <gcsaperas@gmail.com>
2020-08-11 10:59:46 +0200

    Initial commit

---
+++ 01-hello-world/index.php
@@ -0,0 +1,7 @@
+<h1>Hello World! This is HTML</h1>
+
+<?php
+
+	echo '<h2>Hello World from PHP</h2>';	
+
+?>
---
+++ 02-imprimir/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Screen Print - PHP Master</title>
+    </head>
+    <body>
+        <h1>Master in PHP</h1>
+        <?php
+        
+            echo '<h2>Print an H2:</h2>';
+            echo '<ul>'
+                    .'<li>List item 1</li>'
+                    .'<li>List item 2</li>'
+                    .'<li>List item 3</li>'
+                    .'</ul>';
+            
+            echo '<h3>This is a ' . 'concatenation ' . 'example.</h3>';
+        
+        ?>
+        
+        <?= "<p>Echoes out your code automatically.</p>" ?>
+    </body>
+</html>
+<?php
+
+
