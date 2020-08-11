327dc3eb9ef475dacd99f081fa2af6e41b817b1b
Gerard C. Saperas <gcsaperas@gmail.com>
2020-08-11 11:40:31 +0200

    Review of data types in PHP

---
+++ 04-data-types/index.php
@@ -0,0 +1,16 @@
+<?php
+
+// To get the type of a variable -> gettype($variable);
+$num = 100;
+
+echo "The type of the variable \$num is: " . gettype($num) . "<br>";
+
+// Floats' type is "double"
+$float = 1.10;
+
+echo "The type of the variable \$float is: " . gettype($float) . "<br>";
+
+
+// To debug variables... var_dump($variable)
+var_dump($num);
+
