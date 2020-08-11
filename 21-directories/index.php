836de21f6c5838c3b1bffb696a5aa968d2315ff0
Gerard C. Saperas <gcsaperas@gmail.com>
2020-08-11 20:35:57 +0200

    directories with php

--- 21-directories/index.php
+++ 21-directories/index.php
@@ -2,5 +2,22 @@
 
 // Create directory
 // mkdir(dir_name, permissions)
-mkdir('new_folder', 077) or die("Can't create folder");
 
+if (!is_dir('new_folder')) {
+mkdir('new_folder', 077) or die("Can't create folder");
+} else {
+    echo "<p>The folder already exists</p>";
+}
+
+// Remove directory
+// if (is_dir('new_folder')) {
+// rmdir('new_folder') or die("Can't remove folder");
+// } else {
+//    echo "<p>The folder doesn't exists</p>";
+// }
+
+if($manager = opendir('./new_folder')) {
+    while (false !== ($file = readdir('$manager'))) {
+        echo $file;
+    }
+}
